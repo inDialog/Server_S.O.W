2628df17ece013fc1e0e371c431474d97b9c57d1
Calin <calin.segal@in-dialog.com>
2020-04-12 20:40:34 +0200

    targget

--- src/unity/Assets/Scripts/Interactions/Interactions.cs
+++ src/unity/Assets/Scripts/Interactions/Interactions.cs
@@ -8,13 +8,23 @@ public class Interactions : MonoBehaviour
 
     Vector3 Pos_FirstPerson = new Vector3(0, 0, 0.59f);
     Vector3 scale = new Vector3(0.5f, 0.5f, 0.5f);
-    public int oldMask;
+    GameObject _cameraObject;
     Vector3 originalPos;
-    float originalFOV;
-    public GameObject _cameraObject;
     Camera mainCamera;
-    bool alreadyPassed;
+    float originalFOV;
+    int oiginalMask;
 
+    bool alreadyPassed;
+    bool highQualety;
+
+    //private void Start()
+    //{
+    //    _cameraObject = GameObject.FindGameObjectWithTag("MainCamera");
+    //    mainCamera = _cameraObject.GetComponent<Camera>();
+    //    originalPos = _cameraObject.transform.localPosition;
+    //    originalFOV = mainCamera.fieldOfView;
+    //    oldMask = mainCamera.cullingMask;
+    //}
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player" & alreadyPassed == false)
@@ -23,31 +33,30 @@ public class Interactions : MonoBehaviour
             mainCamera = _cameraObject.GetComponent<Camera>();
             originalPos = _cameraObject.transform.localPosition;
             originalFOV = mainCamera.fieldOfView;
-            oldMask = mainCamera.cullingMask;
-
+            oiginalMask = mainCamera.cullingMask;
             alreadyPassed = true;
         }
     }
     ///---- execute change
     private void OnTriggerStay(Collider other)
     {
-        if (action == "FP")
+        if (other.tag == "Player")
         {
-            ZoomIn(_cameraObject);
-        }
-        else
+            if (action == "FP") ZoomIn(_cameraObject);
+            else
             if (action == "Scale") ScaleDown(_cameraObject);
+        }
     }
     ///--- revert to before state
     private void OnTriggerExit(Collider other)
     {
-        if (action == "FP")
+        if (other.tag == "Player")
         {
-            StartCoroutine(ZoomOut(_cameraObject));
-        }
-        else
+            if (action == "FP")
+                StartCoroutine(ZoomOut(_cameraObject));
+            else
             if (action == "Scale") StartCoroutine(ScaleUp(_cameraObject));
-
+        }
     }
     ///////// --- Functions
     ///
@@ -67,24 +76,28 @@ public class Interactions : MonoBehaviour
             {
                 break;
             }
-            mainCamera.cullingMask = oldMask;
             yield return null;
         }
     }
     void ZoomIn(GameObject camera)
     {
-        Debug.Log("Xoom");
-        float speed = 3 * Time.deltaTime;
-        camera.transform.localPosition = Vector3.MoveTowards(camera.transform.localPosition, Pos_FirstPerson, speed);
-        mainCamera.cullingMask = 1 << 0;
+        if (camera.transform.localPosition != Pos_FirstPerson)
+        {
+            mainCamera.cullingMask = 0;
+            camera.GetComponent<StructuredVolumeSampling>().enabled = false;
+            Debug.Log(mainCamera.cullingMask);
+            float speed = 10 * Time.deltaTime;
+            camera.transform.localPosition = Vector3.MoveTowards(camera.transform.localPosition, Pos_FirstPerson, speed);
+        }
     }
     private IEnumerator ZoomOut(GameObject camera)
     {
         while (true)
         {
-            float speed = 3 * Time.deltaTime;
+            camera.GetComponent<StructuredVolumeSampling>().enabled = true;
+            mainCamera.cullingMask = oiginalMask;
+            float speed = 10 * Time.deltaTime;
             camera.transform.localPosition = Vector3.MoveTowards(camera.transform.localPosition, originalPos, speed);
-            mainCamera.cullingMask = oldMask;
 
             if (camera.transform.localPosition == originalPos)
             {
