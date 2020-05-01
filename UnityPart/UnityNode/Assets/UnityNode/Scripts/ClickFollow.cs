7818ba7b59ffbda8f6439640e74b2cc3a1c84cf8
Calin <calin.segal@in-dialog.com>
2020-05-01 20:01:40 +0200

    fdf

--- UnityPart/UnityNode/Assets/UnityNode/Scripts/ClickFollow.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-public class ClickFollow : MonoBehaviour, IClickable {
-    void IClickable.OnClick(RaycastHit hit)
-    {
-		Debug.Log("Following " + hit.collider.gameObject.name);
-    }
-}
